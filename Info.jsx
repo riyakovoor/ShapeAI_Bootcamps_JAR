b52f25b689203275102324fd4209749937656ec4
riyakovoor <44945619+riyakovoor@users.noreply.github.com>
2021-06-28 11:10:31 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>JAVASCRIPT</h1>
+      <p>Well we did leaarn a lot in our life</p>
+    </div>
+    
+  );
+}
+
+export default Info;
